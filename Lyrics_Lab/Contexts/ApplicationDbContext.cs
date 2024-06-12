62fbeebd17d8083ed4b419e7ef489927f1e2c855
Ramon de Oliveira Araújo Mateus <ramonmateus00@gmail.com>
2024-06-12 10:37:56 -0300

    DbContext and create db/first migration

---
+++ Lyrics_Lab/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Lyrics_Lab.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lyrics_Lab.Contexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { }
+
+        public DbSet<Playlist> Playlists => Set<Playlist>();
+        public DbSet<Song> Songs => Set<Song>();
+    }
+}
--- Lyrics_Lab/Program.cs
+++ Lyrics_Lab/Program.cs
@@ -1,6 +1,11 @@
+using Lyrics_Lab.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite("Data Source=lyrics.sqlite3"));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
