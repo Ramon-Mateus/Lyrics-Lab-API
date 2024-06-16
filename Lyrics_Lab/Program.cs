39e4b7f86f2957d9cfd3a3fc2afc99010f143091
Ramon de Oliveira Araújo Mateus <ramonmateus00@gmail.com>
2024-06-16 04:22:48 -0300

    feat: get playlists working

--- Lyrics_Lab/Program.cs
+++ Lyrics_Lab/Program.cs
@@ -5,7 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite("Data Source=lyrics.sqlite3"));
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
