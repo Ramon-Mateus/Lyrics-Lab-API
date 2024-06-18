6bb6faea3b75a3151a3bb557b5f5b04636dcd83d
Ramon de Oliveira Araújo Mateus <ramonmateus00@gmail.com>
2024-06-18 05:02:11 -0300

    att: playlist Id in song

---
+++ Lyrics_Lab/Migrations/20240618080116_Playlisy_Id_Null.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lyrics_Lab.Migrations
+{
+    /// <inheritdoc />
+    public partial class Playlisy_Id_Null : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Songs_Playlists_PlaylistId",
+                table: "Songs");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PlaylistId",
+                table: "Songs",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Songs_Playlists_PlaylistId",
+                table: "Songs",
+                column: "PlaylistId",
+                principalTable: "Playlists",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Songs_Playlists_PlaylistId",
+                table: "Songs");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PlaylistId",
+                table: "Songs",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Songs_Playlists_PlaylistId",
+                table: "Songs",
+                column: "PlaylistId",
+                principalTable: "Playlists",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- Lyrics_Lab/Models/Song.cs
+++ Lyrics_Lab/Models/Song.cs
@@ -6,5 +6,6 @@
         public string Name { get; set; } = string.Empty;
         public string? Lyric { get; set; }
         public DateTime CreatedAt { get; set; }
+        public int? PlaylistId { get; set; }
     }
 }
