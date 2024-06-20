11a4a96d74ebbc809d0f6e242558f4e57f1672d9
Ramon de Oliveira Araújo Mateus <ramonmateus00@gmail.com>
2024-06-20 04:11:34 -0300

    feat: cors settings

--- Lyrics_Lab/Program.cs
+++ Lyrics_Lab/Program.cs
@@ -1,13 +1,18 @@
 using Lyrics_Lab.Contexts;
 using Lyrics_Lab.Data;
 using Lyrics_Lab.Helpers;
+using Lyrics_Lab.Models;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.EntityFrameworkCore;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddCors();
+
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
@@ -27,6 +32,13 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(options => options
+    .WithOrigins(new[] {"http://localhost:3000", "http://localhost:8080", "http://localhost:4200"}) // Colocar a(s) porta(s) que o front-end vai utilizar para permitir acesso à API
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowCredentials()
+);
 
 app.UseAuthorization();
 
